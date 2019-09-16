b898bc0cc5e0e57b9501f16710d89f04e82205e8
longarms07 <ecridd@gmail.com>
2019-09-15 20:39:29 -0400

    Initial input test commit

---
+++ Touch Input Test/Assets/Scripts/TouchCursor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCursor : MonoBehaviour
+{
+
+
+    public GameObject touchCursorPrefab;
+    public int fadeSpeed;
+
+    private bool touchEnded;
+    private GameObject currentCursor;
+    private List<GameObject> pastCursors;
+    private List<SpriteRenderer> pastCursorSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pastCursors = new List<GameObject>();
+        pastCursorSprites = new List<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //
+
+
+}
---
+++ Touch Input Test/Assets/Scripts/TouchInputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TouchInputManager : MonoBehaviour
+{
+
+    public GameObject displayText;
+
+    private TextMeshProUGUI display;
+    static TouchInputManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null) { instance = this; }
+        else { Destroy(this); }
+        display = displayText.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string text = "Size of Input.touches " + Input.touches.Length;
+        if (display != null) { display.text = text; }
+
+    }
+
+    public static TouchInputManager getInstance()
+    {
+        return instance;
+    }
+}
