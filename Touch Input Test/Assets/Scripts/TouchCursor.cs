4ab6dcc6e35be1ced10fabf435a3646d348d13c2
longarms07 <ecridd@gmail.com>
2019-09-16 13:39:27 -0400

    Continued work on TouchInputManager and TouchCursor
    Set up TouchManager to track touch movement and create touch cursors. Set up touch cursors to hopefully handle appearance properly, but  haven't gotten them on screen yet.

--- Touch Input Test/Assets/Scripts/TouchCursor.cs
+++ Touch Input Test/Assets/Scripts/TouchCursor.cs
@@ -19,15 +19,85 @@ public class TouchCursor : MonoBehaviour
     {
         pastCursors = new List<GameObject>();
         pastCursorSprites = new List<SpriteRenderer>();
+        touchEnded = false;
+        Debug.Log("Created touch cursor object");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fadeCursor();
     }
 
-    //
+    // Creates new cursor, adding the past one to the list if it exisits
+    public void changePosition(Vector3 touchPosition)
+    {
+        if (currentCursor != null) { addPast(); }
+        currentCursor = Instantiate(touchCursorPrefab);
+        currentCursor.transform.position = touchPosition;
+        Debug.Log("Created a new current cursor");
+    }
 
+    //Adds the previous cursor to the list, decreases its scale
+    void addPast()
+    {
+        if (currentCursor != null)
+        {
+            pastCursors.Add(currentCursor);
+            currentCursor.transform.localScale = new Vector3(currentCursor.transform.localScale.x / 2,
+                                                             currentCursor.transform.localScale.y / 2,
+                                                             currentCursor.transform.localScale.z / 2);
+            pastCursorSprites.Add(currentCursor.GetComponent<SpriteRenderer>());
+            Debug.Log("Past cursor is now in the list");
+        }
+    }
+
+    public void endTouch()
+    {
+        Debug.Log("Touch has ended");
+        touchEnded = true;
+        addPast();
+        currentCursor = null;
+    }
+
+    //Go through the list, and fade the previous cursors at the given rate. When it's invisible remove them from the list.
+    void fadeCursor()
+    {
+        List<int> toRemove = new List<int>();
+        for(int i = 0; i < pastCursors.Count; i++)
+        {
+            SpriteRenderer sprite = pastCursorSprites[i];
+            float newAlpha = sprite.color.a - fadeSpeed;
+            if (newAlpha > 0)
+            {
+                sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.g, sprite.color.a - fadeSpeed);
+            }
+            else
+            {
+                toRemove.Add(i);
+            }
+        }
+        foreach(int i in toRemove)
+        {
+            GameObject pastCursor = pastCursors[i];
+            pastCursors.Remove(pastCursor);
+            SpriteRenderer sprite = pastCursorSprites[i];
+            pastCursorSprites.Remove(sprite);
+            Destroy(pastCursor);
+        }
+        if(pastCursors.Count == 0 && touchEnded)
+        {
+            destroyThis();
+        }
+    }
+
+    private void destroyThis()
+    {
+        if(touchEnded && currentCursor == null && pastCursors.Count == 0)
+        {
+            Debug.Log("Destroying this...");
+            Destroy(this.gameObject);
+        }
+    }
 
 }
--- Touch Input Test/Assets/Scripts/TouchInputManager.cs
+++ Touch Input Test/Assets/Scripts/TouchInputManager.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class TouchInputManager : MonoBehaviour
 {
 
     public GameObject displayText;
+    public GameObject touchCursorPrefab;
+    public int fingersSupported;
 
+    private TouchCursor[] touchCursors;
     private TextMeshProUGUI display;
     static TouchInputManager instance;
 
@@ -17,12 +21,39 @@ public class TouchInputManager : MonoBehaviour
         if (instance == null) { instance = this; }
         else { Destroy(this); }
         display = displayText.GetComponent<TextMeshProUGUI>();
+        touchCursors = new TouchCursor[fingersSupported];
     }
 
     // Update is called once per frame
     void Update()
     {
-        string text = "Size of Input.touches " + Input.touches.Length;
+        string text = "Size of Input.touches " + Input.touchCount;
+        for(int i=0; i<Math.Min(Input.touchCount, touchCursors.Length); i++)
+        {
+            Touch touch = Input.touches[i];
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchCursors[i] = Instantiate(touchCursorPrefab).GetComponent<TouchCursor>();
+                Debug.Log("Touch Event " + i + " Began");
+            }
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+            {
+                if (touchCursors[i] != null && touch.phase == TouchPhase.Moved)
+                {
+                    touchCursors[i].changePosition(touch.position);
+                }
+                text += "\n Touch " + i + " at Position " + touch.position;
+            }
+            else
+            {
+                if (touchCursors[i] != null) { 
+                    touchCursors[i].endTouch();
+                    touchCursors[i] = null;
+                    Debug.Log("Touch Event "+ i+" Ended");
+                }
+            text += "\n Touch " + i + " Ended.";
+            }
+        }
         if (display != null) { display.text = text; }
 
     }
