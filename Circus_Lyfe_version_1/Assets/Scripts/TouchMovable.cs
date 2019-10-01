8fcb83a6ccba66b7ea50aad83e2a2ef567c4840c
longarms07 <ecridd@gmail.com>
2019-10-01 13:24:07 -0400

    Implemented movement toggling in touchmovable

--- Circus_Lyfe_version_1/Assets/Scripts/TouchMovable.cs
+++ Circus_Lyfe_version_1/Assets/Scripts/TouchMovable.cs
@@ -15,7 +15,10 @@ public class TouchMovable : MonoBehaviour, IDragListener, ITapListener
     public float runThreshold;
     [Tooltip("The gameobject with the collision box to use for detecting valid tap locations.")]
     public GameObject tapCollider;
-    
+
+
+
+    private bool canMove;
     private TouchInputManager touchInputManager;
     private Rigidbody2D da_Rigidbody;
     private bool drag = false;
@@ -23,6 +26,7 @@ public class TouchMovable : MonoBehaviour, IDragListener, ITapListener
     private Vector3 targetPosition;
     private TouchCursor touchCursor;
     private BoxCollider2D tapColliderRB;
+    
     
     
     void Start()
@@ -40,41 +44,49 @@ public class TouchMovable : MonoBehaviour, IDragListener, ITapListener
         else
         {
             da_Rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
-            touchInputManager.SubscribeTapListener(this, touchToFollow);
-            touchInputManager.SubscribeDragListener(this, touchToFollow);
+            ToggleMovement(true);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (drag) MoveTowards(targetPosition);
-        if (tap)
+        if (canMove)
         {
-            MoveTowards(targetPosition);
-            if (this.gameObject.transform.localPosition == targetPosition) TapEnded();
+            if (drag) MoveTowards(targetPosition);
+            if (tap)
+            {
+                MoveTowards(targetPosition);
+                if (this.gameObject.transform.localPosition == targetPosition) TapEnded();
+            }
         }
     }
 
 
     public void DragStarted(Vector3[] dragPositions)
     {
-        if (tap) TapEnded();
-        drag = true;
-        targetPosition = dragPositions[1];
-        MoveTowards(targetPosition);
+        if (canMove)
+        {
+            if (tap) TapEnded();
+            drag = true;
+            targetPosition = dragPositions[1];
+            MoveTowards(targetPosition);
+        }
     }
 
     public void DragPoisitonChanged(Vector3[] dragPositions)
     {
-        targetPosition = dragPositions[1];
-        MoveTowards(targetPosition);
+        if (canMove)
+        {
+            targetPosition = dragPositions[1];
+            MoveTowards(targetPosition);
+        }
     }
 
     public void DragEnded(Vector3[] dragPositions, List<Vector2> deltaPositions)
     {
         drag = false;
-        TapDetected(dragPositions[1]);
+        if(canMove) TapDetected(dragPositions[1]);
     }
 
     public void TouchStarted(Vector3 startPosition)
@@ -84,16 +96,19 @@ public class TouchMovable : MonoBehaviour, IDragListener, ITapListener
 
     public void TapDetected(Vector3 position)
     {
-        if (tap) TapEnded();
-        if (CheckValid(position))
+        if (canMove)
         {
-            targetPosition = position;
-            tap = true;
-            MoveTowards(targetPosition);
-            touchCursor = Instantiate(touchInputManager.touchCursorPrefab).GetComponent<TouchCursor>();
-            touchCursor.changePosition(targetPosition);
+            if (tap) TapEnded();
+            if (CheckValid(position))
+            {
+                targetPosition = position;
+                tap = true;
+                MoveTowards(targetPosition);
+                touchCursor = Instantiate(touchInputManager.touchCursorPrefab).GetComponent<TouchCursor>();
+                touchCursor.changePosition(targetPosition);
+                //Needs pathfinding
+            }
         }
-        //Needs pathfinding
     }
 
     public void TapEnded()
@@ -105,32 +120,56 @@ public class TouchMovable : MonoBehaviour, IDragListener, ITapListener
 
     public void MoveTowards(Vector3 moveTo)
     {
-        Vector3 moveTowards;
-        //Check if we should walk (in loop) or run (out of loop)
-        /*Debug.Log("X difference = " + (moveTo.x - this.gameObject.transform.localPosition.x)
-            + " , Y difference - " + (moveTo.y - this.gameObject.transform.localPosition.y));*/
-        if (Math.Abs(moveTo.x - this.gameObject.transform.localPosition.x) < runThreshold
-            && Math.Abs(moveTo.y - this.gameObject.transform.localPosition.y) < runThreshold)
+        if (canMove)
         {
-            moveTowards = Vector3.MoveTowards(this.gameObject.transform.localPosition, moveTo, Time.fixedDeltaTime * walkSpeed);
-        }
-        else
-        {
-            moveTowards = Vector3.MoveTowards(this.gameObject.transform.localPosition, moveTo, Time.fixedDeltaTime * runSpeed);
-        }
+            Vector3 moveTowards;
+            //Check if we should walk (in loop) or run (out of loop)
+            /*Debug.Log("X difference = " + (moveTo.x - this.gameObject.transform.localPosition.x)
+                + " , Y difference - " + (moveTo.y - this.gameObject.transform.localPosition.y));*/
+            if (Math.Abs(moveTo.x - this.gameObject.transform.localPosition.x) < runThreshold
+                && Math.Abs(moveTo.y - this.gameObject.transform.localPosition.y) < runThreshold)
+            {
+                moveTowards = Vector3.MoveTowards(this.gameObject.transform.localPosition, moveTo, Time.fixedDeltaTime * walkSpeed);
+            }
+            else
+            {
+                moveTowards = Vector3.MoveTowards(this.gameObject.transform.localPosition, moveTo, Time.fixedDeltaTime * runSpeed);
+            }
 
-        da_Rigidbody.MovePosition(moveTowards);
+            da_Rigidbody.MovePosition(moveTowards);
+        }
     }
 
     private Boolean CheckValid(Vector3 position)
     {
-        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down);
-        if(hit.collider!=null && hit.collider == tapColliderRB)
+        if (canMove) { 
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down);
+            if (hit.collider != null && hit.collider == tapColliderRB)
+            {
+                return true;
+            }
+        }
+        return false;
+    
+    }
+
+    public void ToggleMovement(bool toggle)
+    {
+        if (toggle)
         {
-            return true;
+            canMove = true;
+            touchInputManager.SubscribeTapListener(this, touchToFollow);
+            touchInputManager.SubscribeDragListener(this, touchToFollow);
+        }
+        else
+        {
+            canMove = false;
+            touchInputManager.UnsubscribeTapListener(this, touchToFollow);
+            touchInputManager.UnsubscribeDragListener(this, touchToFollow);
+            if (tap) TapEnded();
+            if (drag) DragEnded(null, null);
         }
 
-        return false;
     }
 
 }
