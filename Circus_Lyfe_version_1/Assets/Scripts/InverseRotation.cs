cf1776f265af9971cc4f731b8a4bbf99a0433f35
longarms07 <ecridd@gmail.com>
2019-11-08 17:06:56 -0500

    Tested hinge joints, started adding player character for testing

---
+++ Circus_Lyfe_version_1/Assets/Scripts/InverseRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseRotation : MonoBehaviour
+{
+    public bool active;
+
+    Quaternion lastRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastRotation = gameObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            Quaternion r = gameObject.transform.rotation;
+            Quaternion difference = new Quaternion(r.x - lastRotation.x, r.y - lastRotation.y, r.z - lastRotation.z, r.w - lastRotation.w);
+            //difference = Quaternion.Inverse(difference);
+            lastRotation = new Quaternion(lastRotation.x, lastRotation.y, lastRotation.z - difference.z, lastRotation.w);
+
+            gameObject.transform.rotation = lastRotation;
+        }
+    }
+}
---
+++ Circus_Lyfe_version_1/Assets/Scripts/TrapezeLineRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapezeLineRenderer : MonoBehaviour
+{
+    LineRenderer ln;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ln = gameObject.AddComponent<LineRenderer>();
+        ln.sortingLayerName = "Default";
+        ln.SetWidth(0.1f, 0.1f);
+        ln.SetVertexCount(2);
+        ln.SetPosition(0, gameObject.transform.position);
+        ln.SetPosition(1, target.transform.position);
+        ln.useWorldSpace = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ln.SetPosition(1, target.transform.position);
+    }
+}
