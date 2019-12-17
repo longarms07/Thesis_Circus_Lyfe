9918cb8983eeea7fd6017a5ef902c7692f4848f3
longarms07 <ecridd@gmail.com>
2019-12-17 12:15:25 -0700

    Started turning trapeze into grab target
    Attempted to make a camera follow script, could not decide on a method I was ahppy with so decided against it. Started stting up grab targets on the trapeze, but range detection is not cooperating and is causing a freakout. I believe the fact that the grab target is moving is the likely culprit.

---
+++ Circus_Lyfe_version_1/Assets/Scripts/Trapeze/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject toFollow;
+    public Vector2 constraints;
+    public float maxMove;
+    private float z;
+
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        z = transform.position.z;
+        offset = transform.position - toFollow.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float xDis = Mathf.Abs(transform.position.x - toFollow.transform.position.x);
+        float yDis = Mathf.Abs(transform.position.y - toFollow.transform.position.y);
+        if (xDis >= constraints.x || yDis >= constraints.y) { 
+            Vector3 newPosition = Vector3.MoveTowards(transform.position, toFollow.transform.position+offset, maxMove*Time.deltaTime);
+            newPosition.z = z;
+            transform.position = newPosition;
+         }
+        //transform.position = toFollow.transform.position + offset;
+    }
+}
