afc19d319113bcb981842aac2a82f8a1c555fe77
longarms07 <ecridd@gmail.com>
2020-01-10 16:14:07 -0500

    Added in buildings
    Added art assets for trapeze tent, club, and apartment. Gave them walls and collision boxes, ability to turn invisible when behind them.

---
+++ Circus_Lyfe_version_1/Assets/Scripts/Life Simulation/BuildingIntangibility.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingIntangibility : MonoBehaviour
+{
+
+    public GameObject spriteTarget;
+    public float zAmount;
+    public float opacity;
+    private SpriteRenderer spriteR;
+    private Vector3 setTo;
+    private Vector3 original;
+    private Color oColor;
+    private Color transparent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteR = spriteTarget.GetComponent<SpriteRenderer>();
+        original = spriteTarget.transform.localPosition;
+        setTo = new Vector3(original.x, original.y, zAmount);
+        oColor = spriteR.color;
+        transparent = new Color(oColor.r, oColor.g, oColor.b, opacity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == GameManager.getInstance().playerAvatar)
+        {
+            spriteTarget.transform.localPosition = setTo;
+            spriteR.color = transparent;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameManager.getInstance().playerAvatar)
+        {
+            spriteTarget.transform.localPosition = original;
+            spriteR.color = oColor;
+        }
+    }
+
+
+
+}
