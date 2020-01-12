3c3553e387c5f82b1752863e4f08c5f2d76cd273
longarms07 <ecridd@gmail.com>
2020-01-12 13:41:28 -0500

    Wrote teleportation script, started setting up tent interior

---
+++ Circus_Lyfe_version_1/Assets/Scripts/Life Simulation/TeleportTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTrigger : MonoBehaviour
+{
+
+    public TeleportTrigger target;
+    public bool active;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name + " Entered " + this.gameObject.name);
+        if (collision.gameObject == GameManager.getInstance().playerAvatar && active)
+        {
+            target.Deactivate();
+            collision.gameObject.transform.position = target.transform.position;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameManager.getInstance().playerAvatar)
+        {
+            active = true;
+        }
+    }
+
+    public void Deactivate()
+    {
+        active = false;
+    }
+}
