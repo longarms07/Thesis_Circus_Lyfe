7d655898312ae82019da928ea6dee420a191a4b1
Erik R <38637170+longarms07@users.noreply.github.com>
2021-02-10 13:36:28 -0700

    Created basic title screen

---
+++ Circus_Lyfe_version_1/Assets/Scripts/Utilities/VersionExclusiveText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class VersionExclusiveText : MonoBehaviour
+{
+
+    TextMeshProUGUI tmp;
+    public string WebglText;
+    public string AndroidText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer) tmp.text = WebglText;
+        else if (Application.platform == RuntimePlatform.Android) tmp.text = AndroidText;
+    }
+}
